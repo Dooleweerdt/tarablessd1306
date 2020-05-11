7c92ca4e20099f743bb9725f15d59f92fb8eeacf
Brian Dooleweerdt Rasmussen <brian.rasmussen80@gmail.com>
2020-05-11 20:39:24 +0200

    Changed include of ssd error handler (abort calls). Changed due to obsolete warnings about esp_panic.h.

--- ssd1306_err.h
+++ ssd1306_err.h
@@ -2,7 +2,9 @@
 #define _SSD1306_ERR_H_
 
 #include <esp_log.h>
-#include <esp_panic.h>
+#if CONFIG_SSD1306_ERROR_ABORT
+#include <esp_debug_helpers.h>
+#endif
 
 #if CONFIG_SSD1306_ERROR_ABORT
     #define SSD1306_DoAbort( ) abort( )
